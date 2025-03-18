c8107ef07651d730cddf077672dfc60686c29b6b
LunarcatOwO <168307748+MelodyDevelopment@users.noreply.github.com>
2025-03-18 16:44:33 -0400

    Update contact message

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -6,7 +6,7 @@ return (
   <>
     <div className='w-full flex flex-col items-center justify-center'>
         <h1 className="text-3xl font-bold">Contact Me</h1>
-        <p className="text-lg">You can reach me in the <a href='https://discord.gg/cUXuE6Qmrt' target='_blank' rel='noreferrer' className='text-accent hover:text-red-700'>pyro</a> discord server!</p>
+        <p className="text-lg">You can't contact me yet dummy</p>
     </div>
   </>
 );
